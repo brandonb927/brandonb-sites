ef31e0ea6bf19d874f3fa5f1f49a64a07c373287
Brandon Brown <brandon@brandonb.io>
2016-02-09 13:03:43 -0800

    fix issue with browserync local server

--- gulp_tasks/config/dev.js
+++ gulp_tasks/config/dev.js
@@ -1,8 +1,8 @@
 // Development config
 
 import { existsSync } from 'fs'
-import { extname, resolve } from 'path'
-import url from 'url'
+import { extname, join, resolve } from 'path'
+import { parse } from 'url'
 import { merge } from 'lodash'
 
 import baseConfig from './base'
@@ -23,10 +23,10 @@ const baseDevConfig = {
       baseDir: build,
       middleware: [
         (req, res, next) => { // middleware for clean, extensionless URLs
-          let uri = url.parse(req.url)
+          let uri = parse(req.url)
           if (uri.pathname.length > 1 &&
               extname(uri.pathname) === '' &&
-              existsSync(`${path.join(build, uri.pathname)}.html`)) {
+              existsSync(`${join(build, uri.pathname)}.html`)) {
             req.url = `${uri.pathname}.html${uri.search || ''}`
           }
           next()
