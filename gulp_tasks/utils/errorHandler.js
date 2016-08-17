8ec649dfd4dd5b06d2265053788d0dbf5bc2d838
Brandon Brown <brandon@brandonb.io>
2016-08-17 00:04:16 -0700

    Finally fix gulp-plumber bug

--- gulp_tasks/utils/errorHandler.js
+++ gulp_tasks/utils/errorHandler.js
@@ -1,9 +1,14 @@
 import gutil from 'gulp-util'
 import notify from 'gulp-notify'
 
-export default (err) => {
-  gutil.log(err)
-  gutil.beep()
+export default function (err) {
+  let args = Array.prototype.slice.call(arguments)
+
+  // Send error to notification center with gulp-notify
+  notify.onError({
+    title: "Compile Error",
+    message: "<%= error.message %>"
+  }).apply(this, args)
 
   // Keep gulp from hanging on this task
   this.emit('end')
