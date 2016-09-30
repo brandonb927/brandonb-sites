6322c197aa2d9cb1acf6925cf5473798cb607d38
Brandon Brown <brandon@brandonb.io>
2016-09-29 22:56:36 -0700

    Add a much-needed flag to not rebuild the APIs on every run

--- gulp_tasks/tasks/build.js
+++ gulp_tasks/tasks/build.js
@@ -1,29 +1,54 @@
+import { argv } from 'yargs'
 import gulp from 'gulp'
 import runSequence from 'run-sequence'
 
 // Run the build
 gulp.task('build:dev', (callback) => {
-  runSequence(
-    'delete:dev',
-    'jekyll-build:dev',
-    [
-      'styles:dev',
-      'scripts:dev',
-      'copy:dev'
-    ],
-    callback
-  )
+  if (argv.update_apis) {
+    runSequence(
+      'delete:dev',
+      'jekyll-build:dev',
+      [
+        'styles:dev',
+        'scripts:dev',
+        'copy:dev'
+      ],
+      callback
+    )
+  } else {
+    runSequence(
+      'jekyll-build:dev',
+      [
+        'styles:dev',
+        'scripts:dev',
+        'copy:dev'
+      ],
+      callback
+    )
+  }
 })
 
 gulp.task('build:prod', (callback) => {
-  runSequence(
-    'delete:prod',
-    'jekyll-build:prod',
-    [
-      'styles:prod',
-      'scripts:prod',
-      'copy:prod'
-    ],
-    callback
-  )
+  if (argv.update_apis) {
+    runSequence(
+      'delete:prod',
+      'jekyll-build:prod',
+      [
+        'styles:prod',
+        'scripts:prod',
+        'copy:prod'
+      ],
+      callback
+    )
+  } else {
+    runSequence(
+      'jekyll-build:prod',
+      [
+        'styles:prod',
+        'scripts:prod',
+        'copy:prod'
+      ],
+      callback
+    )
+  }
 })
--- gulp_tasks/tasks/default.js
+++ gulp_tasks/tasks/default.js
@@ -1,16 +1,25 @@
+import { argv } from 'yargs'
 import gulp from 'gulp'
 import runSequence from 'run-sequence'
 
 // Build the development environment, and watch files for changes
 gulp.task('default', (callback) => {
-  runSequence(
-    'build:dev',
-    'browser-sync',
-    [
+  if (argv.update_apis) {
+    runSequence(
+      'build:dev',
+      'browser-sync',
       'foursquare:dev',
-      'instagram:dev'
-    ],
-    'watch',
-    callback
-  )
+      'instagram:dev',
+      'watch',
+      callback
+    )
+  } else {
+    runSequence(
+      'build:dev',
+      'browser-sync',
+      'watch',
+      callback
+    )
+  }
+
 })
