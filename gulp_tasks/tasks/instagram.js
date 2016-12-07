978fb5b0d0d0bfe018de14aa8d7c8921ab5d4846
Brandon Brown <brandon@brandonb.io>
2016-12-06 17:51:58 -0800

    Cleanup Instagram build process a bit

--- gulp_tasks/tasks/instagram.js
+++ gulp_tasks/tasks/instagram.js
@@ -16,50 +16,63 @@ import configProd from '../config/prod'
 const INSTAGRAM_OAUTH_TOKEN = process.env.INSTAGRAM_OAUTH_TOKEN
 
 // Set the API Base URL, straight forward
-const API_BASE_URL = "https://api.instagram.com/v1/users/self/media/recent"
+const API_BASE_URL = "https://api.instagram.com/v1/"
 
-const API_ENDPOINT = `${API_BASE_URL}?access_token=${INSTAGRAM_OAUTH_TOKEN}`
-const LOGGED_API_ENDPOINT = `${API_BASE_URL}?access_token=XXXXXX`
+// Log messaging
+const ERROR_MSG = '✗ Request to Instagram failed'
+const SUCCESS_MSG = '✓ Hitting Instagram API:'
+
+// Generate an API url
+const generateApiUrl = (url) => {
+  let full_url =  `${API_BASE_URL}${url}?access_token=${INSTAGRAM_OAUTH_TOKEN}`
+
+  // Make the full_url safe for logging to stdout, etc.
+  let safe_url = full_url.replace(INSTAGRAM_OAUTH_TOKEN, 'XXXXXX')
+
+  gutil.log(gutil.colors.green(SUCCESS_MSG, safe_url))
+
+  return full_url
+}
+
+// Set the recent media path
+const PATH_GET_RECENT_MEDIA = 'users/self/media/recent'
 
 const buildJSON = (buildPath, callback) => {
   let imageItems = {}
   let folderPath = `${buildPath}/api`
   let filePath = `${folderPath}/instagram.json`
-  let errorMsg = '✗ Request to Instagram failed'
-  let successMsg = '✓ Hitting Instagram API:'
 
   // Get recent Instagram photos
-  gutil.log(gutil.colors.green(successMsg, LOGGED_API_ENDPOINT))
+  let recentMediaRequest = request('GET', generateApiUrl(PATH_GET_RECENT_MEDIA))
+  let recentMediaResponse = JSON.parse(recentMediaRequest.getBody('utf8'))
 
-  let res = request('GET', API_ENDPOINT)
-  let response = JSON.parse(res.getBody('utf8'))
-
-  if (response.meta.code !== 200) {
-    gutil.log(gutil.colors.red(errorMsg))
+  if (recentMediaResponse.meta.code !== 200) {
+    gutil.log(gutil.colors.red(ERROR_MSG))
     return
   }
 
-  for (let item of response.data) {
-    if (item.type === 'image') {
-      imageItems[item.id] = {
-        type: item.type,
-        url: item.link,
-        image: item.images.standard_resolution.url,
-        height: item.images.standard_resolution.height,
-        width: item.images.standard_resolution.width,
-        caption: item.caption.text,
-        tags: item.tags,
-        likes: item.likes.count,
+  for (let media of recentMediaResponse.data) {
+    // TODO: Add comments in the future?
+    if (media.type === 'image') {
+      imageItems[media.id] = {
+        type: media.type,
+        url: media.link,
+        image: media.images.standard_resolution.url,
+        height: media.images.standard_resolution.height,
+        width: media.images.standard_resolution.width,
+        caption: media.caption.text,
+        tags: media.tags,
+        likes: media.likes.count,
       }
     }
-    else if (item.type === 'video') {
-      imageItems[item.id] = {
-        type: item.type,
-        url: item.link,
-        video: item.videos.standard_resolution.url,
-        caption: item.caption.text,
-        tags: item.tags,
-        likes: item.likes.count,
+    else if (media.type === 'video') {
+      imageItems[media.id] = {
+        type: media.type,
+        url: media.link,
+        video: media.videos.standard_resolution.url,
+        caption: media.caption.text,
+        tags: media.tags,
+        likes: media.likes.count,
       }
     }
 
@@ -72,62 +85,6 @@ const buildJSON = (buildPath, callback) => {
 
   // 'touch' the file first so it exists when writing to it
   closeSync(openSync(filePath, 'w'))
-
-  // // Instagram says you can only retrieve a max of 250 items per response, so we go with that
-  // let maxApiRequests = 250
-  //
-  // // We want to use the total checkin count to use with paginatation math
-  // let totalApiCheckins = data.response.checkins.count
-  //
-  // // Get the total (ceiling) pages we can call to the api
-  // let totalApiCheckinPages = Math.ceil(totalApiCheckins / maxApiRequests)
-  //
-  // // Start the paging offset at 0
-  // let paginationOffset = 0
-  //
-  // // Get the checkins and page through them
-  // let i = 0
-  // while (i < totalApiCheckinPages) {
-  //   // Build the paging URL to use with the API call
-  //   let API_URL = `${API_ENDPOINT}&limit=${maxApiRequests}&offset=${paginationOffset}`
-  //   let result = request('GET', API_URL)
-  //   let responseData = JSON.parse(result.getBody('utf8'))
-  //
-  //   let LOGGED_API_URL = `${LOGGED_API_ENDPOINT}&limit=${maxApiRequests}&offset=${paginationOffset}`
-  //   gutil.log(gutil.colors.green(successMsg, LOGGED_API_URL))
-  //
-  //   if (responseData.meta.code !== 200) {
-  //     gutil.log(gutil.colors.red(errorMsg))
-  //     return
-  //   }
-  //
-  //   // Build a giant object of image data
-  //   for (let checkin of checkinData) {
-  //     let venue = checkin.venue
-  //
-  //     if (!imageItems.hasOwnProperty(venue.id)) {
-  //       imageItems[venue.id] = {
-  //         'venue': venue,
-  //         'checkins': []
-  //       }
-  //     }
-  //
-  //     imageItems[venue.id]['checkins'].push(checkin)
-  //   }
-  //
-  //   // Sort dates of checkins, descending by date
-  //   for (let venueId in imageItems) {
-  //     imageItems[venueId]['checkins'].sort(function(a, b) {
-  //       a = new Date(a.createdAt);
-  //       b = new Date(b.createdAt);
-  //       return a>b ? -1 : a<b ? 1 : 0;
-  //     })
-  //   }
-  //
-  //   // Bump the offset to use for paging the API
-  //   paginationOffset += maxApiRequests
-  //   i++
-  // }
 
   // Write the JSON data to the file finally
   let jsonData = JSON.stringify(imageItems)
