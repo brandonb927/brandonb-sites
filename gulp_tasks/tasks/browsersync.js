90863e7b69c6562b5d932a0122ba3fdd41888cfe
Brandon Brown <brandon@brandonb.ca>
2017-11-22 17:27:54 -0800

    Add ngrok support with browsersync

--- gulp_tasks/tasks/browsersync.js
+++ gulp_tasks/tasks/browsersync.js
@@ -1,7 +1,19 @@
 import gulp from 'gulp'
 import browsersync from 'browser-sync'
+import ngrok from 'ngrok'
+
 import config from '../config/dev'
 
 gulp.task('browser-sync', () => {
-  browsersync(config.browsersync)
+  browsersync(
+    config.browsersync,
+    (err, bs) => {
+      ngrok.connect(
+        bs.options.get('port'),
+        (err, url) => {
+          console.log(`Your ngrok URL is: ${url}`)
+        }
+      )
+    }
+  )
 })
