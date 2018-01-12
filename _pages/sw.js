19e2789e420cb08704b424bd7c5a10403250f4a5
Brandon Brown <brandon@brandonb.ca>
2018-01-12 10:10:11 -0800

    Fix the serviceworker not phasing out old versions of posts

--- _pages/sw.js
+++ _pages/sw.js
@@ -8,67 +8,71 @@ var CACHE_NAME = "brandonb-cache::{{site.time | date: '%Y%m%d%H%M%S'}}"
 var urlsToCache = []
 
 // Cache posts
-{% for post in site.posts %}
-urlsToCache.push('{{ post.url }}')
+{% for post in site.posts %}urlsToCache.push('{{ post.url }}')
 {% endfor %}
 
 // Cache pages
 // Conditionally removes JS, TXT, and XML files from the loop as well
-{% for page in site.pages %}
-{% unless page.url contains '.js' or page.url contains '.txt' or page.url contains '.xml' %}
-urlsToCache.push('{{ page.url }}')
-{% endunless %}
-{% endfor %}
+{% for page in site.pages %}{% unless page.url contains '.js' or page.url contains '.txt' or page.url contains '.xml' %}urlsToCache.push('{{ page.url }}')
+{% endunless %}{% endfor %}
 
 // Cache assets
 urlsToCache.push('/assets/styles/site.css')
 
 self.addEventListener('install', event => {
-  // Perform install steps
   event.waitUntil(
     caches
       .open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache')
         return cache.addAll(urlsToCache)
       })
   )
 })
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return
+  }
+
   event.respondWith(
     caches
-      .open(CACHE_NAME)
-      .then(cache => {
-        return cache
-          .match(event.request)
-          .then(response => {
-            return response || fetch(event.request).then(response => {
-              cache.put(event.request, response.clone())
+      .match(event.request)
+      .then((cached) => {
+        var networked = fetch(event.request)
+          .then((response) => {
+            var cacheCopy = response.clone()
+
+            caches
+              .open(CACHE_NAME)
+              .then((cache) => {
+                return cache.put(event.request, cacheCopy)
+              })
+
               return response
-            })
           })
-        })
-        .catch(() => {
-          // Fallback to the offline page if not available in the cache.
-          return caches.match('/offline')
-        })
+          .catch(() => {
+            return caches.match('/offline')
+          })
+
+        return cached || networked
+      })
   )
 })
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
     caches
-      .open(CACHE_NAME)
-      .then(cache => {
-        return fetch(event.request).then(response => {
-          cache.put(event.request, response.clone())
-          return response
-        })
-      })
-      .catch(() => {
-        // Fallback to the offline page if not available in the cache.
-        return caches.match('/offline')
+      .keys()
+      .then((keys) => {
+        return Promise.all(
+          keys
+            .filter((key) => {
+              return !key.startsWith(CACHE_NAME)
+            })
+            .map((key) => {
+              return caches.delete(key)
+            })
+        )
       })
   )
 })
