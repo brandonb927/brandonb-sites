8ca45c95d1e2d0bb1adec3434014492b4215e499
Brandon Brown <brandon@brandonb.ca>
2020-01-17 09:07:27 -0800

    Start building link shortener

---
+++ gulp_tasks/tasks/shorten_url.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp'
+
+gulp.task('shorten_url', () => {
+/*
+  Outline:
+
+  - Create new md file with redirect_to template
+
+  - Detect of user-defined short url is used, or to generate a random one within bounds
+
+  const template = `
+  ---
+  layout: redirect_to
+  title: <OPTIONAL TITLE>
+  redirect_to: <URL>
+  permalink: /<PATH> <ONLY INCLUDE IF USER DEFINED PATH PROVIDED>
+  sitemap: false
+  ---
+  `;
+
+  - Provide output with a URLs for local and live server (also maybe ngrok?)
+  - Make sure this is proper-formed URL
+*/
+})
