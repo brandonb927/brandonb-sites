8d3fe4d391d8bdd87ea74429ecf7db559036ef8f
Brandon Brown <brandonb@echosec.net>
2021-01-09 18:08:47 -0800

    Updates Flickr task to finally take into account cached data and only fetch recent photos
    Also updates Flickr data

--- gulp_tasks/flickr.js
+++ gulp_tasks/flickr.js
@@ -5,6 +5,11 @@ const _ = require('lodash')
 const fs = require('fs')
 const Flickr = require('flickr-sdk')
 
+const DATA_FILE_PATH = './_data/flickr-data.json'
+const MIN_DATE_TAKEN = '2019-01-01 00:00:00'
+
+const CACHED_DATA = getCachedData()
+
 var flickr = new Flickr(
   Flickr.OAuth.createPlugin(
     process.env.FLICKR_CONSUMER_KEY,
@@ -14,28 +19,69 @@ var flickr = new Flickr(
   )
 )
 
-async function oauthFlow() {
-  var oauth = new Flickr.OAuth(
-    process.env.FLICKR_CONSUMER_KEY,
-    process.env.FLICKR_CONSUMER_SECRET
-  )
+// Use to get OAuth token
+// async function oauthFlow() {
+//   var oauth = new Flickr.OAuth(
+//     process.env.FLICKR_CONSUMER_KEY,
+//     process.env.FLICKR_CONSUMER_SECRET
+//   )
 
-  const oauthReponse = await oauth.request('http://localhost:8888')
-  const { oauth_token: oauthToken } = oauthReponse.body
+//   const oauthReponse = await oauth.request('http://localhost:8888')
+//   const { oauth_token: oauthToken } = oauthReponse.body
+//   const authorizationUrl = await oauth.authorizeUrl(oauthToken)
+//   open(authorizationUrl)
+// }
 
-  const authorizationUrl = await oauth.authorizeUrl(oauthToken)
+function getCachedData() {
+  // Check if the cached date file exists
+  if (fs.existsSync(DATA_FILE_PATH)) {
+    // Read the cached date file and query the most recent photo taken date
+    return JSON.parse(fs.readFileSync(DATA_FILE_PATH))
+  }
 
-  open(authorizationUrl)
+  return null
+}
+
+async function cacheLicenses() {
+  const licensesResponse = await flickr.photos.licenses.getInfo()
+  const { license } = licensesResponse.body.licenses
+
+  const formattedLicenses = {}
+  license.forEach(({ id, name: licenseName }) => {
+    formattedLicenses[id] = licenseName
+  })
+
+  return formattedLicenses
 }
 
 async function getPhotos() {
   console.log('Retrieving photos from Flickr...')
 
+  // Use a default minimum date to start with
+  let minDateTaken = MIN_DATE_TAKEN
+
+  const cachedData = CACHED_DATA
+
+  // Check if the cached date file exists
+  if (cachedData) {
+    // Read the cached date file and query the most recent photo taken date
+    const recentYear = cachedData[0]
+    const recentMonth = Object.keys(recentYear.months)[0]
+    const recentPhoto = recentYear.months[recentMonth][0]
+
+    // Get the most recent date and add a day so when we query it doesn't retrieve old data
+    const parsedDate = new Date(Date.parse(recentPhoto.date_taken))
+    // parsedDate.setDate(parsedDate.getDate() + 1)
+
+    // Reset the min_date_taken string to our most recent date
+    minDateTaken = `${parsedDate.toISOString().substring(0, 10)} 00:00:00`
+  }
+
   const photosResponse = await flickr.people.getPhotos({
     user_id: 'me',
     per_page: 500,
-    extras: 'date_taken,description,tags,url_c,url_o',
-    min_taken_date: '2019-01-01 00:00:00',
+    extras: 'date_taken,description,license,tags,url_c,url_o',
+    min_taken_date: minDateTaken,
     content_type: '1',
   })
   return photosResponse.body
@@ -79,21 +125,47 @@ async function getPhotoExifData(photoId) {
 
 async function fetchAndWriteFlickrData() {
   const { photos } = await getPhotos()
+
+  const licenses = await cacheLicenses()
+
   const flickrData = []
 
   for (let i in photos.photo) {
-    const { id, datetaken, description, tags, url_o, url_c } = photos.photo[i]
+    const {
+      id,
+      datetaken,
+      description,
+      license: licenseId,
+      tags,
+      title,
+      url_o,
+      url_c,
+    } = photos.photo[i]
 
     const exif = await getPhotoExifData(id)
 
+    let completeDescription = ''
+    if (title) {
+      if (title && description._content) {
+        completeDescription = `${title}\n${description._content}`
+      } else {
+        completeDescription = title
+      }
+    }
+
+    if (description._content) {
+      completeDescription = description._content
+    }
+
     flickrData.push({
+      date_taken: new Date(datetaken),
+      description: completeDescription,
+      exif,
       id,
-      description: description._content,
+      license: licenses[licenseId],
       tags,
       src_original: url_o,
       src: url_c,
-      date_taken: new Date(datetaken),
-      exif,
     })
   }
 
@@ -123,14 +195,38 @@ async function fetchAndWriteFlickrData() {
     rebuiltData[year][monthName].push(photo)
   }
 
+  const cachedData = CACHED_DATA
   const sortedData = []
+
+  if (cachedData) {
+    // Take the cached data and merge it into our rebuilt data
+    // so we can create a new sorted data file
+    cachedData.forEach(obj => {
+      const cachedYear = obj.year
+
+      if (!rebuiltData.hasOwnProperty(cachedYear)) {
+        rebuiltData[cachedYear] = {}
+      }
+
+      const cachedMonths = Object.keys(obj.months)
+
+      for (let cachedMonth of cachedMonths) {
+        if (!rebuiltData[cachedYear].hasOwnProperty(cachedMonth)) {
+          rebuiltData[cachedYear][cachedMonth] = obj.months[cachedMonth]
+        } else {
+          rebuiltData[cachedYear][cachedMonth].concat(obj.months[cachedMonth])
+        }
+      }
+    })
+  }
+
   Object.keys(rebuiltData)
     .reverse()
     .forEach(year => {
       sortedData.push({ year, months: rebuiltData[year] })
     })
 
-  fs.writeFileSync('./_data/flickr-data.json', JSON.stringify(sortedData))
+  fs.writeFileSync(DATA_FILE_PATH, JSON.stringify(sortedData))
 
   console.log('Done!')
 }
