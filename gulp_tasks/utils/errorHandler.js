813bc1ca223bfcf826b5209c86353687acc362dd
Brandon Brown <brandonb@echosec.net>
2021-12-13 13:51:28 -0800

    Removes gulp-notify

--- gulp_tasks/utils/errorHandler.js
+++ gulp_tasks/utils/errorHandler.js
@@ -1,15 +1,6 @@
-import notify from 'gulp-notify'
-
 export default function(err) {
-  let args = Array.prototype.slice.call(arguments)
-
   // Send error to notification center with gulp-notify
-  notify
-    .onError({
-      title: 'Compile Error',
-      message: '<%= error.message %>',
-    })
-    .apply(this, args)
+  console.error(err.message, `\n  Filename: ${err.fileName}`)
 
   // Keep gulp from hanging on this task
   this.emit('end')
