5fed943066e83fa5fa04737bc097c37f1c778ba3
Brandon Brown <brandon@brandonb.ca>
2024-08-06 13:50:27 -0700

    Move prettier config to separate file

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 export default [
   {
-    parserOptions: {
-      ecmaVersion: 2024,
-      sourceType: 'module',
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+      },
     },
     files: ['*/_assets/scripts/**/*.js'],
   },
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+}
